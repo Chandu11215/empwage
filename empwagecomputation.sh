f0135d4a3cb321da625faf1475a1d1d0c9065d75
chandu11215 <kalalshannu19@gmail.com>
2020-05-23 16:29:03 +0530

    empwagecomputation

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -1,40 +1,17 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation"
-declare -A employeeDailyWage
-IS_FULL_TIME=1
-IS_PART_TIME=2
-MAX_WORKING_DAYS=20
-MAX_WORKING_HOURS=100
+empAttendance=$(( RANDOM%2 ))
+IS_PRESENT=1
 WAGE_PER_HOUR=20
-employeeDailyHours=0
+employeeDailyWage=0;
 employeeWorkHours=0
-employeeWorkDays=0
-employeeMonthlyWage=0
+if [ $empAttendance -eq $IS_PRESENT ]
+then
+	employeeWorkHours=8
+else
+	employeeWorkHours=0
+fi
 
-function dailyWorkHours()
-{
-	case $1 in
-		$IS_PART_TIME)
-			echo 4
-		;;
+employeeDailyWage=$(( WAGE_PER_HOUR*employeeWorkHours))
 
-		$IS_FULL_TIME)
-			echo 8
-		;;
-
-		*)
-			echo 0
-		;;
-	esac
-}
-while  (( employeeWorkHours<MAX_WORKING_HOURS && employeeWorkDays<MAX_WORKING_DAYS ))
-do
-	employeeAttendance=$(( RANDOM%3 ))
-	employeeDailyHours=$( dailyWorkHours $employeeAttendance )
-	employeeWorkHours=$((employeeWorkHours+employeeDailyHours))
-	((employeeWorkDays++))
-	employeeDailyWage["Day"$employeeWorkDays]=$((WAGE_PER_HOUR*employeeDailyHours))
-	employeeMonthlyWage=$((employeeMonthlyWage+employeeDailyWage["Day"$employeeWorkDays]))
-done
-echo ${!employeeDailyWage[@]} ${employeeDailyWage[@]}
